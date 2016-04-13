5cbea206d1bd1cbc23b9529ad0749f0d2380653c
Carola <Carola>
2016-04-12 23:57:53 -0400

    Juego version 1.1

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ Hernán Cortés ID 1056025
 Miguel Oviedo ID 1053442
 */
 
+var from;
+var fuerza;
 var Space = new cp.Space();
+
 Space.gravity =cp.v(0,-100);
 Space.sleepTimeThreshold = 0.5;
 Space.colisionSlop = 0.5;
@@ -23,38 +26,100 @@ var HelloWorldLayer = cc.Layer.extend({
     leftWall:null,
     
     crash: function(){
-        var bird = this.BirdNode.getBoundingBox();
-        for(var node of this.cajas){
-            var b = node.getBoundingBox();
-            if(cc.rectIntersectsRect(bird,b)){
-                this.BirdNode.stopAllActions();
-            }
-        }
-    },
-    
-    crash2: function(){
     var bird = this.BirdNode.getBoundingBox();
       for(var pgnode of this.cerdos){
             var b = pgnode.getBoundingBox();
             if(cc.rectIntersectsRect(bird,b)){
-                this.BirdNode.stopAllActions();
+                this.removeChild(pgnode,true);
+            }
+            
+            if(this.BirdNode.getPositionY() < 100){
+                this.BirdNode.setPosition(150,200);
+            }
+          
+            if(this.cerdos.length == 0){
+                cc.alert("You win!");
             }
         }  
+    },
+    
+    began:function(location,event){
+        var game  = event.getCurrentTarget();
+        var loc = location.getLocation();
+        from = loc;
+        cc.log("from "+from.x+" "+from.y);
+        
+        return true;
+    },
+    
+    cals: function(location, event){
+        var game  = event.getCurrentTarget();
+        var loc = location.getLocation();
+        
+        fuerza = loc;
+        
+        fuerza.x = fuerza.x-from.x;
+        fuerza.y = fuerza.y-from.y;
+        cc.log("to "+fuerza.x+" "+fuerza.y);
+        
     },
     
     move: function(location,event){
         var game  = event.getCurrentTarget();
         var loc = location.getLocation();
-        var fly = cc.JumpTo.create(3,cc.p(700,300),300,1);
-       
-        game.BirdNode.runAction(fly);
+
+        if(fuerza.x<100){
+            if(fuerza.y<100){
+                var fly = cc.JumpTo.create(2,cc.p(400,100),100,1);
+                game.BirdNode.runAction(fly);
+            }
+            if(fuerza.y===100){
+                var fly = cc.JumpTo.create(2,cc.p(500,100),200,1);
+                game.BirdNode.runAction(fly);
+            }
+            if(fuerza.y>100){
+                var fly = cc.JumpTo.create(2,cc.p(600,100),300,1);
+                game.BirdNode.runAction(fly);
+            }
+        }
+        
+        if(fuerza.x===100){
+            if(fuerza.y<100){
+                var fly = cc.JumpTo.create(1,cc.p(600,100),300,1);
+                game.BirdNode.runAction(fly);
+            }
+            if(fuerza.y===100){
+                var fly = cc.JumpTo.create(2,cc.p(700,100),400,1);
+                game.BirdNode.runAction(fly);
+            }
+            if(fuerza.y>100){
+                var fly = cc.JumpTo.create(2,cc.p(800,100),500,1);
+                game.BirdNode.runAction(fly);
+            }
+        }
+        
+        if(fuerza.x>100){
+            if(fuerza.y<100){
+                var fly = cc.JumpTo.create(2,cc.p(700,100),500,1);
+                game.BirdNode.runAction(fly);
+            }
+            if(fuerza.y===100){
+                var fly = cc.JumpTo.create(2,cc.p(800,100),600,1);
+                game.BirdNode.runAction(fly);
+            }
+            if(fuerza.y>100){
+                cc.log("fly away!");
+                var fly = cc.JumpTo.create(2,cc.p(900,100),700,1);
+                game.BirdNode.runAction(fly);
+            }
+            
+        }
     },
     
     pig : function(x,y){
-        for(var i=0;i<2;i++){
+        for(var i=0;i<1;i++){
             var pig = new cc.Sprite(res.pig);
             var pigz = pig.getContentSize(),
-                
             pgnode = cc.PhysicsSprite.create(res.pig),
             pgbody = null,
             pgShape = null,
@@ -69,38 +134,37 @@ var HelloWorldLayer = cc.Layer.extend({
 
             pgShape = Space.addShape(new cp.CircleShape(pgbody,pigz.width *0.5,cc.p(0,0)));
             pgShape.setFriction(0.5);
-            pgShape.setElasticity(1);
+            pgShape.setElasticity(0);
             pgShape.setCollisionType(2);
 
             pgnode.setBody(pgbody);
             pgnode.setRotation(0);
             pgnode.setScale(0.05);
             
-            this.cerdos.push(pgnode);
-            
+            this.cerdos.push(pgnode); 
             this.addChild(pgnode);
         }
     },
     
-    boxes: function(x,y){
-        mass = 1;
-        for (var i = 0;i<4;i++){
+    boxes: function(x,y,amount){
+        mass = 5;
+        
+        for (var i = 0;i<amount;i++){
             var box = new cc.Sprite(res.box);
             var bg = box.getContentSize(),
                 node = cc.PhysicsSprite.create(res.box),
                 bodys = null,
                 Shape = null,
-                scaleX =1,
-                scaleY =1;
+                scaleX =0.5,
+                scaleY =0.5;
             
             bg.width *= scaleX;
             bg.height *=scaleY;
-            factor = 1.5;
             
             bodys = new cp.Body(mass, cp.momentForBox(mass, bg.width, bg.height));
             
             if(i>0){
-               y=y*factor;
+               y=y+150;
             }
             
             bodys.setPos(cc.p(x,y));
@@ -113,10 +177,11 @@ var HelloWorldLayer = cc.Layer.extend({
 
             node.setBody(bodys);
             node.setRotation(0);
-            node.setScale(1);
+            node.setScale(0.5);
             
             this.cajas.push(node);
             this.addChild(node);
+                
         }  
     },
     
@@ -132,49 +197,49 @@ var HelloWorldLayer = cc.Layer.extend({
         this.sprite = new cc.Sprite(res.background);
         this.sprite.setPosition(size.width, size.height);
         this.addChild(this.sprite, 0);
-        var mass = 5;
         
-        this.boxes(500,200);
-        this.boxes(800,200);
-        this.boxes(900,200);
+        var mass = 10;
+        
+        this.boxes(500,200,6);
+        this.boxes(800,200,6);
         
         this.redBird=new cc.Sprite(res.red);
         
-        this.pig(500,400);
-        this.pig(800,400);
-        this.pig(900,400);
+        this.pig(750,400);
+        this.pig(600,400);
+        this.pig(700,400);
+        this.pig(750,600);
+        this.pig(600,600);
         this.pig(700,600);
-        this.pig(800,600);
-        this.pig(900,600);
         
         var BirdSize = this.redBird.getContentSize();
         this.BirdNode = cc.PhysicsSprite.create(res.red);
         var BirdBody = null;
         var BirdShape = null;
-        var scaleX = 0.03;
-        var scaleY = 0.03;
+        var scaleX = 0.02;
+        var scaleY = 0.02;
         
         BirdSize.width *= scaleX;
         BirdSize.height *= scaleY;
-        
+
         BirdBody = Space.addBody(new cp.Body(mass, cp.momentForBox(mass, BirdSize.width, BirdSize.height)));
         BirdBody.setPos(cc.p(150,200));
 
         BirdShape = Space.addShape(new cp.CircleShape(BirdBody, BirdSize.width * 0.5, cc.p(0, 0)));
         BirdShape.setFriction(0.5);
-        BirdShape.setElasticity(1);
+        BirdShape.setElasticity(2);
         BirdShape.setCollisionType(2);
 
         this.BirdNode.setBody(BirdBody);
         this.BirdNode.setRotation(100);
-        this.BirdNode.setScale(0.03);
+        this.BirdNode.setScale(0.02);
 
         this.addChild(this.BirdNode);
         
         var update = function(){
             Space.step(1/60);
         }
-        
+
         this.leftWall = new cp.SegmentShape(Space.staticBody, new cp.v(0, 0), new cp.v(0, cc.winSize.height), 5);
         this.leftWall.setElasticity(0.5);
         this.leftWall.setFriction(1);
@@ -194,16 +259,21 @@ var HelloWorldLayer = cc.Layer.extend({
         this.upperWall = new cp.SegmentShape(Space.staticBody, new cp.v(0, cc.winSize.height), new cp.v(0, cc.winSize.height), 5);
         this.upperWall.setElasticity(0.5);
         this.upperWall.setFriction(1);
-        
         Space.addStaticShape(this.upperWall);
+
         Space.addCollisionHandler(1,2,null,null,null,null);
         
         this.schedule(update);
         
         cc.eventManager.addListener({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            onTouchBegan: this.move
+            onTouchBegan: this.began,
+            onTouchMoved: this.cals,
+            onTouchEnded: this.move,
+            
         },this);
+
+        this.schedule(this.crash,0);
         
         return true;
     }
@@ -216,4 +286,3 @@ var HelloWorldScene = cc.Scene.extend({
         this.addChild(layer);
     }
 });
-
